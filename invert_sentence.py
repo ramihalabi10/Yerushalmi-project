8547912ea3038f2aaca108df9379e1102c4595d7
ramihalabi10 <118489512+ramihalabi10@users.noreply.github.com>
2024-01-16 20:04:43 +0200

    Add files via upload

---
+++ invert_sentence.py
@@ -0,0 +1,17 @@
+import numpy
+import string
+
+
+
+def invert_message(message):
+
+    inverted = ''
+    inverted=message[::-1]
+
+    return inverted
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(invert_message("סלאח עבאס שונה הפעלה,"))
+
+
